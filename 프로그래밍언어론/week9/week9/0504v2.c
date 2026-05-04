73cc44beae72c97a7fbb97c44a38f2cdfdc4c9cf
junny96859706 <junny96859706@gmail.com>
2026-05-04 13:06:22 +0900

    프언9주차

---
+++ 프로그래밍언어론/week9/week9/0504v2.c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int* a = (int*)malloc(sizeof(int));
+	int* b = (int*)malloc(sizeof(int));
+
+	*a = 1;
+	*b = 2;
+	printf("*a = %d : *b = %d\n", *a, *b);
+
+	a = b;
+	printf("*a = %d : *b = %d\n", *a, *b);
+}
