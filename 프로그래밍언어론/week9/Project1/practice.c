a60aa4a1a68f6738c29c9e2d0febfe42f787a395
junny96859706 <junny96859706@gmail.com>
2026-05-04 13:17:11 +0900

    프언9주차업로드 수정

---
+++ 프로그래밍언어론/week9/Project1/practice.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+
+int A;
+
+void sub(){
+	static int j = 1;
+	int k = 1;
+
+	printf("j = %d, k = %d\n", j, k);
+	j++;
+	k++;
+}
+
+int main() {
+	int a;
+
+	sub();
+	sub();
+	sub();
+}
