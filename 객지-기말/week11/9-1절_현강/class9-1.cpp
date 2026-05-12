b01e65239622c774b1b764e61a4fe75a1b795f8c
junny96859706 <junny96859706@gmail.com>
2026-05-12 17:31:48 +0900

    26/5/12 객지11주차 실강 수업내용 공부

---
+++ 객지-기말/week11/9-1절_현강/class9-1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+class Base {
+public:
+	virtual void draw() {
+		cout << "Base::f()" << endl;
+	}
+};
+
+class Derived : public Base {
+public:
+	virtual void draw() {	
+		cout << "Derived::f()" << endl;
+	}
+};
+
+int main() {
+	Derived d;
+	d.draw();
+
+	Derived* p;
+	p = &d;
+	p->draw();
+
+	Base* q;
+	q = &d; //업캐스팅
+	q->draw();
+}
---
+++ 객지-기말/week11/9-2절_현강/class9-2.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+using namespace std;
+
+class Shape { //추상클래스
+	Shape* next;
+protected:
+	virtual void draw() = 0; //순수 가상 함수 선언
+public:
+	Shape() { next = NULL; }
+	void paint() {
+		draw();
+	}
+	Shape* add(Shape* p) {
+		next = p;
+		return p;
+	}
+	Shape* getNext(){ return next; }
+};
+
+class Line : public Shape {
+protected:
+	virtual void draw() { cout << "Line" << endl; }
+};
+
+class Rect : public Shape {
+protected:
+	virtual void draw() { cout << "Rect" << endl; }
+};
+
+class Circle : public Shape {
+protected:
+	virtual void draw() { cout << "Circle" << endl; }
+};
+
+void drawAll(Shape* p) {
+	while (p != NULL) {
+		p->paint();
+		p = p->getNext();
+	}
+}
+
+int main() {
+	Shape* pstart = NULL;
+	Shape* pLast = NULL;
+
+	pstart = new Circle();
+	pLast = pstart;
+
+	pLast = pLast->add(new Rect());
+	pLast = pLast->add(new Line());
+	pLast = pLast->add(new Circle());
+
+	drawAll(pstart);
+}
