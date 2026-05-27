e44fd19b8b3cc7bd109ae66e3b9be1db16ba892c
junny96859706 <junny96859706@gmail.com>
2026-05-28 04:10:32 +0900

    프랩 및 객지문제출이

--- 객지-기말/week13/실습문제11-6/ascii.cpp
+++ 객지-기말/week13/실습문제11-6/ascii.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <iomanip>
 #include <cctype> //isprint함수 사용을 위한 헤더 선언
 using namespace std;
 
@@ -6,5 +7,34 @@ using namespace std;
 //문자로 출력 가능하지 않는 아스키 코드는 '.'으로 출력하라. (문제11-6번)
 
 int main() {
+
+	for (int i = 0; i < 4; i++) {
+		cout << left << setw(10) << "dec";
+		cout << setw(10) << "hexa";
+		cout << setw(10) << "char";
+	}
+	cout << endl;
+	for (int i = 0; i < 4; i++) {
+		cout << setw(10) << "___";
+		cout << setw(10) << "____";
+		cout << setw(10) << "____";
+	}
+
+	//본문 출력
+	for (int i = 0; i <= 60; i += 4) {
+		for (int j = 0; j < 4; j++) {
+			cout << dec << setw(10) << i+j;
+			cout << hex << setw(10) << i+j;
+			if (isprint(i + j)) {
+				cout << setw(10) << (char)(i + j); //(char)i+j라고 하면 안된다!!! i만 형변환 한다.
+			}
+			else
+				cout << setw(10) << ".";
+		}
+		cout << endl;
+	}
+
+	cout << dec << right << setfill('-') << setw(60) << "이하생략";
+	cout << left << setfill('-') << setw(60); //setfill()도 계속 유지되서 굳이 안넣어도 된다!!!!
 
 }
---
+++ 프로그래밍랩/week13/아스키코드 출력 및 색상바꾸기/ascii.c
@@ -0,0 +1,76 @@
+﻿#include <stdio.h>
+#include <Windows.h>
+
+#define WIDTH 10
+#define HEIGHT 5
+
+void DrawCharacter(int posY, HANDLE hConsole) {
+	char pixel = 219;
+
+	char screen[HEIGHT][WIDTH] = {
+		{0,0,pixel,pixel,pixel,pixel,0,0,0,0},
+		{0,pixel,0,0,0,0,pixel,0,0,0},
+		{pixel,0,pixel,0,0,pixel,0,pixel,0,0},
+		{pixel,0,0,0,0,0,0,pixel,0,0},
+		{0,pixel,pixel,pixel,pixel,pixel,pixel,0,0,0}
+	};
+
+	for (int i = 0; i < posY; i++) {
+		printf("\n");
+	}
+
+	for (int y = 0; y < HEIGHT; y++) {
+		for (int x = 0; x < WIDTH; x++) {
+			if (screen[y][x] == 0) {
+				printf(" ");
+			}
+			else {
+				SetConsoleTextAttribute(hConsole, 10);
+				printf("%c", screen[y][x]);
+			}
+		}
+		printf("\n");
+	}
+}
+
+int main() {
+	//콘솔화면을 제어할수 있는 핸들을 가져와라
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	SetConsoleOutputCP(437); //아스키코트를 출력할수 있게끔 설정
+
+	//커서 숨기기
+	CONSOLE_CURSOR_INFO cursorinfo = { 1,FALSE };
+	SetConsoleCursorInfo(hConsole, &cursorinfo);
+	
+	int posY = 0;
+	int direction = 1;
+
+	while (1) {
+		system("cls"); //화면지우기
+		DrawCharacter(posY, hConsole);
+
+		posY += direction;
+
+		if (posY >= 3)
+			direction = -1;
+		if (posY <= 0)
+			direction = 1;
+
+		Sleep(100);
+		
+	}
+
+	/*for (int i = 1; i <= 15; i++) {
+		SetConsoleTextAttribute(hConsole, i);
+
+		for (int j = 0; j < 5; j++) {
+			printf("%c", pixel);
+		}
+		printf("Color %d\n", i);
+	}
+	*/
+	SetConsoleTextAttribute(hConsole, 7);
+		
+	return 0;
+}
