1267de4fb97ce49f7f54fde3aa4702b922fd02e9
Junny96859706 <junny96859706@gmail.com>
2026-06-03 04:00:59 +0900

    객치 10장 실습문제 풀이

--- 객지-기말/week12/실습문제7번/lab10-7.cpp
+++ 객지-기말/week12/실습문제7번/lab10-7.cpp
@@ -2,81 +2,109 @@
 #include <vector>
 using namespace std;
 
-int biggest(vector<int> &v) {
-	int temp = v[0];
+//1. 데이터와 기능을 하나로 묶는 클래스 선언
+class VectorManager {
+    //원본 데이터는 외부로부터 철저히 보호.
+    vector<int> array;
+    vector<int>::iterator it;
 
-	for (int i = 0; i < v.size(); i++) {
-		if (temp < v[i])
-			temp = v[i];
-	}
-	return temp;
-}
+    //내부에서만 쓰이는 보조 함수 (const를 붙여 원본 변형 방지)
+    int getBiggest() const {
+        int temp = array[0];
+        for (int i = 0; i < array.size(); i++) {
+            if (temp < array[i]) temp = array[i];
+        }
+        return temp;
+    }
 
+public:
+    //2. 외부(main)에서 호출할 수 있는 공개 기능들 (인터페이스)
+
+    // 초기 10개 입력받기
+    void inputInitialData() {
+        cout << "10개의 정수 입력>>";
+        for (int i = 0; i < 10; i++) {
+            int num; cin >> num;
+            array.push_back(num);
+        }
+    }
+
+    // 1번: 가장 큰 값 출력
+    void printBiggest() const {
+        // 내부 함수인 getBiggest()를 호출해서 결과를 보여줌.
+        cout << "가장 큰 값 " << getBiggest() << endl;
+    }
+
+    // 2번: 평균 출력
+    void printAverage() const {
+        int sum = 0;
+        for (int i = 0; i < array.size(); i++) {
+            sum += array.at(i);
+        }
+        cout << "평균 " << (double)sum / array.size() << endl;
+    }
+
+    // 3번: 삽입
+    void insertValue() {
+        int add;
+        cout << "삽입할 값>>";
+        cin >> add;
+        array.push_back(add);
+    }
+
+    // 4번: 삭제
+    void deleteValue() {
+        int delete_num;
+        cout << "삭제할 값>>";
+        cin >> delete_num;
+        for (int i = 0; i < array.size(); i++) {
+            if (array[i] == delete_num) {
+                it = array.begin() + i;
+                it = array.erase(it);
+                i--;
+            }
+        }
+        printAll(); // 삭제 후 결과 보여주기
+    }
+
+    // 5번: 몇 배로 증가
+    void increaseValues() {
+        int increase;
+        cout << "몇배로 증가>>";
+        cin >> increase;
+        for (int i = 0; i < array.size(); i++) {
+            array.at(i) *= increase;
+        }
+        printAll();
+    }
+
+    // 공통 출력 함수
+    void printAll() const {
+        for (int i = 0; i < array.size(); i++) {
+            cout << array.at(i) << " ";
+        }
+        cout << endl;
+    }
+};
+
+//3. main 함수는 흐름 제어만 담당 (매우 직관적이고 깔끔해짐)
 int main() {
-	vector<int> array;
-	vector<int>::iterator it;
-	int num;
+    VectorManager manager; // 관리자 객체 생성
+    manager.inputInitialData();
 
-	cout << "10개의 정수 입력>>";
-	for (int i = 0; i < 10; i++) {
-		cin >> num;
-		array.push_back(num);
-	}
+    while (true) {
+        int choise;
+        cout << "가장 큰 값:1, 평균:2, 삽입:3, 삭제:4, 증가:5, 종료:6>>";
+        cin >> choise;
 
-	while (true) {
-		int choise;
-		cout << "가장 큰 값:1, 평균:2, 삽입:3, 삭제:4, 증가:5, 종료:6>>";
-		cin >> choise;
-
-		switch(choise){
-		case 1: {//가장큰값
-			cout << "가장 큰 값 " << biggest(array) << endl;
-		}
-		case 2: { //평균
-			int sum = 0;
-			for (int i = 0; i < array.size(); i++) {
-				sum += array.at(i);
-			}
-			cout << "평균 " << double(sum / array.size()) << endl;
-		}
-		case 3: { //삽입
-			int add;
-			cout << "삽입할 값>>";
-			cin >> add;
-			array.push_back(add);
-		}
-		case 4: { //삭제 
-			int delete_num;
-			cout << "삭제할 값>>";
-			cin >> delete_num;
-			for (int i = 0; i < array.size(); i++) {
-				if (array[i] == delete_num) {
-					it = array.begin() + i;
-					it = array.erase(it); //v.erase(it)은 it이 가르키는 원소를 삭제한후 다음원도에 대한 포인터리턴
-				}
-			}
-
-			for (int i = 0; i < array.size(); i++) {
-				cout << array.at(i) << " ";
-			}
-			cout << endl;
-		}
-		case 5: { //증가
-			int increase;
-			cout << "몇배로 증가>>";
-			cin >> increase;
-			for (int i = 0; i < array.size(); i++) {
-				array.at(i) *= increase;
-			}
-			for (int i = 0; i < array.size(); i++) {
-				cout << array.at(i) << " ";
-			}
-			cout << endl;
-		}
-		case 6: { //종료
-			break;
-		}
-		}
-	}
-	return 0;
+        switch (choise) {
+        case 1: manager.printBiggest(); break;
+        case 2: manager.printAverage(); break;
+        case 3: manager.insertValue();  break;
+        case 4: manager.deleteValue();  break;
+        case 5: manager.increaseValues(); break;
+        case 6: cout << "시스템을 종료합니다" << endl; return 0;
+        }
+    }
+    return 0;
 }
---
+++ 객지-기말/week12/실습문제8번/lab10-8.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int biggest(vector<int>& v) {
+	if (v.empty()) return 0; //혹시나 벡터가 비어있을때를 위한 방어코드
+	int temp = v.at(0);
+	for (int i = 0; i < v.size(); i++) {
+		if (temp < v.at(i))
+			temp = v.at(i);
+	}
+	return temp;
+}
+
+void print_array(vector<int>& v) {
+	for (int i = 0; i < v.size(); i++) {
+		cout << v.at(i) << ' ';
+	}
+}
+
+bool check_array(vector<int>& v,int temp) {
+	for (int i = 0; i < v.size(); i++) {
+		if (temp == v.at(i))
+			return true;
+	}
+	return false;
+}
+
+int main() {
+	vector<int> array;
+
+	while (true) {
+		int temp;
+		cout << "정수를 입력하세요. 0이면 종료>>";
+		cin >> temp;
+
+		if (temp == 0)
+			return 0;
+
+		if (check_array(array,temp)) {
+			cout << "이미 있음" << endl;
+			continue;
+		}
+		else array.push_back(temp);
+
+		cout << "[";
+		print_array(array);
+		cout << "], 가장 큰 수" << biggest(array) << endl;
+	}
+}
