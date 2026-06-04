7f7149d94c8b66d788d81eb343f2d98f5981f1ae
junny96859706 <junny96859706@gmail.com>
2026-06-05 00:41:31 +0900

    c++ part10

--- 객지-기말/week10/실습문제6-1번/lab8-6-1.cpp
+++ 객지-기말/week10/실습문제6-1번/lab8-6-1.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <cmath>
 using namespace std;
 
 class Schedule {
@@ -15,7 +16,12 @@ public:
 };
 
 Schedule::Schedule(int curloc, int n[], int count) {
-	this->curLoc = curloc; this->jobs = n; this->count = count;
+	jobs = new int[count]; //동적배열할당
+	for (int i = 0; i < count; i++) {
+		jobs[i] = n[i];
+	}
+
+	this->curLoc = curloc;  this->count = count;
 	cout << "현재 위치는 " << curloc << "동: " << "배달 시작 ..." << curloc;
 	for (int i = 0; i < count; i++) {
 		cout << " -> " << jobs[i];
@@ -25,14 +31,16 @@ Schedule::Schedule(int curloc, int n[], int count) {
 
 int Schedule::run() {
 	int total = 0;
-	total += ((jobs[0] - curLoc) > 0 ? (jobs[0] - curLoc) : -(jobs[0] - curLoc));
+	total += abs(jobs[0] - curLoc);
 	curLoc = jobs[0];
 	for (int i = 1; i < count; i++) {
-		total += ((jobs[i] - curLoc)>0 ? (jobs[i] - curLoc) : -(jobs[i] - curLoc)); //삼항연산자 이용
+		total += (jobs[i] - curLoc); 
 		curLoc = jobs[i];
 	}
 	return total;
 }
+
+//--------------------------------------------------------------------------------//
 
 class ShortestFirst : public Schedule {
 public:
@@ -43,19 +51,53 @@ public:
 
 ShortestFirst::ShortestFirst(int curloc, int n[], int count) 
 	: Schedule(curloc, change_array(curloc,n,count), count) {
-
 }
+
 
 //배열을 ShortestFirst방식에 맞춰서 배열을 재정렬시키는 함수
 int* ShortestFirst::change_array(int curloc, int n[], int count) {
 	//C++ 표준 문법에서 배열의 크기는 변수가 아니라 무조건 상수여야 함. 컴파일 시점에 크기를 알 수 없기 때문
 	//컴파일 에러가 나지 않도록 변수(count) 크기만큼 메모리를 동적 할당.
-	int* temp = new int[count];
+	int* temp = new int[count+1]; //curloc포함 7개변수 저장 가능한 동적배열
+	
+	//1.최솟값으로 정렬하고 curloc값 넣기
+	//이중for문으로 제일 작은값을 배열 처음에 넣어가기
 	for (int i = 0; i < count; i++) {
-		temp[i] = n[i];
+		int min = n[i];
+		int index = i;
+		for (int j = i; j < count; j++) {
+			if (n[j] < min) {
+				min = n[j];
+				index = j;
+			}
+		}
+
+		int temp = n[i];
+		n[i] = n[index];
+		n[index] = temp;
 	}
+	// n= {3,7,9,12,16,25} 이렇게 최솟값부터 정렬시키고 curloc값을 중앙에 넣은다음 양옆을 비교하는 알고리즘
+	//주혁이:배열 하나 만들어서 curLoc - n[i] 뺀값들을 넣어, 그다음에 최소값을 찾아 배열 돌아서 그다음에 그 인덱스를 curLoc으로 하고 jobs에 넣음 이렇게 했음
+
+
+	//2.curloc과 가까운 값을 다음 목적지로 설정하고, 해당목적지를 curloc으로 설정
+	// n= {3,7,9,  [10]  ,12,16,25}
+
+
+	 
+	
+	//3.재정렬된 배열을 기존배열에 복사하고 주소리턴
 	int min = temp[0];
+
+
+
+	for (int i = 0; i < count; i++) {
+		temp[i] = n[i]; //배열복사
+	}
+	return n;
 }
+
+//------------------------------------------------------------------------------------------------//
 
 int main() {
 	int jobs[6];
