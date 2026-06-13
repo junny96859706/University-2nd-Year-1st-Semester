ef92256148aa98fbd7384944c9c32960e148dfb0
junny96859706 <junny96859706@gmail.com>
2026-06-14 03:55:30 +0900

    .

---
+++ 객지-기말/week12/ex10-5/ex10-5.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+using namespace std;
+
+template <class T>
+class Mystack {
+	int top;
+	T data[100];
+public:
+	Mystack();
+	void push(T element);
+	T pop();
+};
+
+template <class T>
+Mystack<T>::Mystack() {
+	top = -1;
+}
+
+template <class T>
+void Mystack<T>::push(T element) {
+	if (top == 99)
+		return;
+	data[++top] = element;
+}
+
+template <class T>
+T Mystack<T>::pop() {
+	if (top == -1)
+		return T(); //T의 기본값(0, NULL 등)을 생성해서 리턴하는 안전한 방법!
+	return data[top--];
+}
+
+int main() {
+	Mystack<int> iStack;
+	iStack.push(3);
+	cout << iStack.pop() << endl;
+
+	Mystack<double> dStack;
+	dStack.push(3.5);
+	cout << dStack.pop() << endl;
+
+	Mystack<char>* p = new Mystack<char>();
+	p->push('a');
+	cout << p->pop() << endl;
+	delete p;
+}
---
+++ 객지-기말/week12/ex10-9/ex10-9.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+
+int main() {
+	map<string, string> dic;
+
+	dic.insert(make_pair("love", "사랑"));
+	dic.insert(make_pair("apple", "사과"));
+	dic["cherry"] = "체리";
+
+	cout << "저장된 단어 개수: " << dic.size() << endl;
+
+	while (true) {
+		string word;
+		cout << "찾고 싶은 단어>> ";
+		getline(cin, word); //<string>헤더파일 있어야 한다!!!
+		if (word == "exit") {
+			break;
+		}
+
+		if (dic.find(word) == dic.end())
+			cout << "없음" << endl;
+		else
+			cout << dic[word] << endl;
+	}
+	cout << "종료합니다..." << endl;
+}
